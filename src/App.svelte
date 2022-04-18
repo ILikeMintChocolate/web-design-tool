459bd3d9ca8efe7ae4c5e7503477c67bb3909461
ILikeMintChocolate <99123542+ILikeMintChocolate@users.noreply.github.com>
2022-04-18 17:52:37 +0900

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	import EditorPage from "./Page/EditorPage.svelte";
+
+
+    if (import.meta.hot) {
+        import.meta.hot.on(
+            "vite:beforeUpdate",
+            () => console.clear()
+        );
+    }
+
+</script>
+
+<main>
+	<EditorPage></EditorPage>
+</main>
+
+<style>
+	main {
+        margin: 0;
+        padding: 0;
+	}
+</style>
